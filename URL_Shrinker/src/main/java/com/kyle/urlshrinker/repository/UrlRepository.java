7b0bdf550fbc642a71fa825d54579f90c14b2bc5
Kyle Meier <kyle.meier@swisscom.com>
2024-10-29 08:27:03 +0100

    Refactor UrlRepository to include a custom query for finding a Url by its shortCode

--- URL_Shrinker/src/main/java/com/kyle/urlshrinker/repository/UrlRepository.java
+++ URL_Shrinker/src/main/java/com/kyle/urlshrinker/repository/UrlRepository.java
@@ -1,4 +1,14 @@
 package com.kyle.urlshrinker.repository;
 
-public class UrlRepository {
+import com.kyle.urlshrinker.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+
+    @Query("SELECT u FROM Url u WHERE u.shortUrl = :shortCode")
+    Optional<Url> findUrl(@Param("shortCode") String shortCode);
 }
