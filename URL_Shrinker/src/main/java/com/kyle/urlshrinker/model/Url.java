47cd95745dac49116aeb99e5b0ba1e65bdf3d1a8
Kyle Meier <kyle.meier@swisscom.com>
2024-10-29 08:27:42 +0100

    Refactor Url class and update application.properties

--- URL_Shrinker/src/main/java/com/kyle/urlshrinker/model/Url.java
+++ URL_Shrinker/src/main/java/com/kyle/urlshrinker/model/Url.java
@@ -1,16 +1,18 @@
 package com.kyle.urlshrinker.model;
 
 import jakarta.persistence.*;
-
 import lombok.*;
 
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Url {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String originalUrl;
     private String shortUrl;
     private boolean active;
+
+    // Lombok handles the getters, setters and constructors
 }
